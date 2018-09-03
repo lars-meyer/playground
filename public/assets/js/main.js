207ea5e9a79173870ed0927d2fcbd75ed4727ce8
lars-meyer <lars.meyer@ymail.com>
2018-09-03 20:57:12 +0200

    rainy rainbow

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,67 +1,177 @@
 
 this.kontra.init();
-var x;
 
 // vars
-var elemArr = [],
-loop,
-boxWrapper = document.querySelector('.elem'),
-boxWrapperHeight = boxWrapper.clientHeight;
+var moveArr = [],
+    spriteArr = [],
+    goalsprite,
+    loop,
+    // boxWrapper = document.querySelector('.elem'),
+    // boxWrapperHeight = boxWrapper.clientHeight;
+    kontra,
+    spriteY = -30,
+    colorCount = 0;
 
-var boxOffY = 50,
-    boxOffX = 0,
-    boxWidth = 30,
-    boxHeight = 30;
+var staticStuff = createStaticElems();
 
-var createCount = 1,
-    elemCount = 1;
+/**
+* create all static elems
+*
+*/
+function createStaticElems () {
+  // create goal line
+  goalsprite = kontra.sprite({
+    x: 0,
+    y: 130,
+    color: '#000',
+    width: 500,
+    height: 1
+  });
+  spriteArr.push(goalsprite);
 
-// calcs
-if (typeof boxWrapperHeight !== 'undefined') {
-  boxOffY = ( boxWrapperHeight / 4 ) ;
+  return true;
 }
 
-// create
-for (createCount; createCount <= elemCount; createCount++) {
+if (staticStuff) {
+  createMovingElems();
+}
 
-  var sprite = kontra.sprite({
-    x: boxOffX,
-    y: boxOffY,
-    color: 'red',
-    width: boxWidth,
-    height: boxHeight,
-    dx: createCount
-  });
+/**
+* create all moving elems
+*
+*/
+function createMovingElems () {
 
-  elemArr.push(sprite);
+  var movCreateCount = 1,
+      movElemCount = 5;
 
-  if (createCount === elemCount) {
-    addEvents();
+  for (movCreateCount; movCreateCount <= movElemCount; movCreateCount++) {
+
+    var spriteWidth = 10,
+        spriteHeight = 10,
+        spriteX = 5,
+        dropRate = 1;
+
+    if (movCreateCount > 1) {
+      spriteX = spriteX + 10;
+    }
+
+    var sprite = kontra.sprite({
+      x: randomizeVal('offsetX'),
+      y: spriteY,
+      color: randomizeVal('color'),
+      width: spriteWidth,
+      height: spriteHeight,
+      dy: randomizeVal('dropRate')
+    });
+
+    moveArr.push(sprite);
+
+    if (movCreateCount === movElemCount) {
+      addElemsToCanvas();
+    }
   }
 }
 
-function addEvents() {
+function addElemsToCanvas() {
 
   loop = kontra.gameLoop({
+
     update: function() {
-      elemArr.forEach(function(item) {
+      moveArr.forEach(function(item) {
+
         item.update();
-        if (item.x > kontra.canvas.width) {
-          item.x = -boxOffX;
+
+        if (item.y > kontra.canvas.height) {
+          item.y = spriteY;
+          item.x = randomizeVal('offsetX');
+          item.color = randomizeVal('color');
+          item.dy = randomizeVal('dropRate');
         }
-      })
+
+        if (item.collidesWith(goalsprite)) {
+          item.y = spriteY;
+          item.x = randomizeVal('offsetX');
+          item.color = randomizeVal('color');
+          item.dy = randomizeVal('dropRate');
+        }
+
+      });
     },
     render: function() {
-      elemArr.forEach(function(item) {
+      spriteArr.forEach(function(item) {
         item.render();
-      })
+      });
+
+      moveArr.forEach(function(item) {
+        item.render();
+      });
     }
   });
 
   startLoop();
 }
 
-// start anim and add update/looping
 function startLoop() {
   loop.start();
+}
+
+// randomize new values for each mov elem
+function randomizeVal(mod) {
+
+  if (mod === 'offsetX') {
+    // offset X
+    var spriteX = 20,
+        xMinFac = 1,
+        xMaxFac = 13,
+        minX = Math.ceil(xMinFac),
+        maxX = Math.floor(xMaxFac),
+        xFactor = Math.floor(Math.random() * (maxX - minX)) + minX;
+
+    spriteX = spriteX * xFactor;
+    // console.log('spriteX', spriteX);
+    return(spriteX);
+  }
+
+  if (mod === 'dropRate') {
+    // dropRate
+    var minDrop = 1,
+        maxDrop = 4,
+        dropRate = Math.random() * (maxDrop - minDrop) + minDrop,
+        dropRate = dropRate.toFixed(2);
+
+    return(dropRate);
+  }
+
+  if (mod === 'color') {
+
+    var color;
+
+    if (colorCount === 0) {
+      color = 'red';
+      colorCount++;
+    }
+
+    else if (colorCount === 1) {
+      color = 'yellow';
+      colorCount++;
+    }
+
+    else if (colorCount === 2) {
+      color = 'orange';
+      colorCount++;
+    }
+
+    else if (colorCount === 3) {
+      color = 'blue';
+      colorCount++;
+    }
+
+    else {
+      color = 'green';
+      colorCount = 0;
+    }
+
+    return(color);
+  }
 }
